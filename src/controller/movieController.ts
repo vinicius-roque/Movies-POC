14e3d3591b340288bd05a5b86854a6e1af1a311e
Vinícius Roque <viniciusfr6@gmail.com>
2022-11-12 00:26:52 -0300

    feat(): implemented the functionality to show all movies

--- src/controller/movieController.ts
+++ src/controller/movieController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { connection } from '../database/db.js';
+import { Movie } from '../protocol/movie.js';
 
 async function createMovie(req: Request, res: Response) {
     const { name, streaming, genre, status } = req.body;
@@ -12,9 +13,22 @@ async function createMovie(req: Request, res: Response) {
 
         return res.sendStatus(201);
     } catch (error) {
-        console.log(error);
         return res.status(500).send(error.message);
     }
 }
 
-export { createMovie };
+async function listMovies(req: Request, res: Response) {
+    try {
+        const movies: Movie[] = (await connection.query('SELECT * FROM movies;')).rows;
+
+        return res.send(movies);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+async function deleteMovie(req: Request, res: Response) {
+
+}
+
+export { createMovie, listMovies, deleteMovie };
--- src/router/movieRouter.ts
+++ src/router/movieRouter.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { movieValidation } from '../middleware/movieMiddleware.js';
-import { createMovie } from '../controller/movieController.js';
+import { createMovie, listMovies, deleteMovie } from '../controller/movieController.js';
 
 const router = express.Router();
 
-router.post('/movie', movieValidation, createMovie);
+router.post('/movies', movieValidation, createMovie);
+router.get('/movies', listMovies);
+router.delete('/movies/:id', deleteMovie);
 
 export default router;
